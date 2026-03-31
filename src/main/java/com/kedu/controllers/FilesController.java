f45ab7c48892ac6f707683c533159537aadd4697
KingJuYoung <dlawndud6544@gmail.com>
2026-04-01 01:21:29 +0900

    근무지 등록 dao 만드는중

--- src/main/java/com/kedu/controllers/FilesController.java
+++ src/main/java/com/kedu/controllers/FilesController.java
@@ -16,26 +16,26 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/files")
 public class FilesController {
 
-	@PostMapping("/upload")
-	@ResponseBody
-	public Map<String, String> upload(@RequestParam("image") MultipartFile file) throws Exception {
-		System.out.println("도착");
-	    String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-
-	    String path = "C:/upload/";
-	    
-	    File savePathFile = new File(path);
-	    if(!savePathFile.exists()){
-	        savePathFile.mkdirs(); 
-	    }
-	    
-	    File dest = new File(path + fileName);
-	    file.transferTo(dest);
-
-	    Map<String, String> map = new HashMap<>();
-	    map.put("url", "/upload/" + fileName); // ⭐ 이 URL이 핵심
-
-	    return map;
-	}
+//	@PostMapping("/upload")
+//	@ResponseBody
+//	public Map<String, String> upload(@RequestParam("image") MultipartFile file) throws Exception {
+//		System.out.println("도착");
+//	    String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+//
+//	    String path = "C:/upload/";
+//	    
+//	    File savePathFile = new File(path);
+//	    if(!savePathFile.exists()){
+//	        savePathFile.mkdirs(); 
+//	    }
+//	    
+//	    File dest = new File(path + fileName);
+//	    file.transferTo(dest);
+//
+//	    Map<String, String> map = new HashMap<>();
+//	    map.put("url", "/upload/" + fileName); // ⭐ 이 URL이 핵심
+//
+//	    return map;
+//	}
 	
 }
--- src/main/java/com/kedu/dao/WorkPlaceDAO.java
+++ src/main/java/com/kedu/dao/WorkPlaceDAO.java
@@ -1,5 +1,19 @@
 package com.kedu.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.kedu.dto.WorkPlaceDTO;
+
 public class WorkPlaceDAO {
 
+	@Autowired
+	public JdbcTemplate jdbc;
+	
+	public int insertToworkpalce(WorkPlaceDTO dto) {
+		String sql = "insert into workplace values ()";
+		return jdbc.update(sql);
+	}
+	
+	
 }
