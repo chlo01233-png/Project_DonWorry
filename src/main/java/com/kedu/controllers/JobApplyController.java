7427b3c5e5c812e58162df6a1eea87ce5f791245
킹주영 <dlawndud6544@naver.com>
2026-04-09 16:54:56 +0900

    pull 전 커밋

--- src/main/java/com/kedu/controllers/JobApplyController.java
+++ src/main/java/com/kedu/controllers/JobApplyController.java
@@ -1,5 +1,7 @@
 package com.kedu.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kedu.dao.JobApplyDAO;
 import com.kedu.dao.ResumeDAO;
+import com.kedu.dto.ResumeDTO;
 
 @Controller
 @RequestMapping("/jobapplys")
@@ -31,9 +34,25 @@ public class JobApplyController {
 		
 		int resumeNum = rdao.getPrimaryResume(loginId);
 		
+		if (resumeNum == 0) {
+		    rattr.addFlashAttribute("resume", "먼저 이력서를 작성해주세요! 이력서 작성으로 이동합니다.");
+		    return "redirect:/jobposts/jobpost";
+		}
+		
 		dao.insert(loginId, resumeNum, jobPostNum);
 		rattr.addFlashAttribute("message", "지원이 완료되었습니다.");
 		
 		return "redirect:/jobposts/jobpost";
 	}
+	
+//	@RequestMapping("/getMyResumes")
+//	public String @ResponseBody // 페이지 이동이 아니라 데이터를 보내야 하므로 필수!
+//	@RequestMapping("/getMyResumes")
+//	public List<ResumeDTO> getMyResumes(HttpSession session) {
+//	    String loginId = (String)session.getAttribute("loginId");
+//	    // 해당 사용자의 모든 이력서를 가져오는 DAO 메서드 호출
+//	    return rdao.selectMyResumes(loginId); 
+//	}() {
+//		
+//	}
 }
