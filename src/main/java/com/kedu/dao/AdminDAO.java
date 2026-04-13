00833e14dd5ca38424886a35c59846e1f672afd8
jimin <a@b.com>
2026-04-13 20:14:58 +0900

    rpl

--- src/main/java/com/kedu/dao/AdminDAO.java
+++ src/main/java/com/kedu/dao/AdminDAO.java
@@ -276,7 +276,7 @@ public class AdminDAO {
 	               + "    r.seq, r.parent_seq, m.nickname AS member_id, "
 	               + "    r.content, r.write_date, r.re_reply_seq, r.member_id AS writer, "
 	               + "    COUNT(rp.seq) AS report_count, "
-	               + "    LISTAGG(rp.reason, ', ') WITHIN GROUP (ORDER BY rp.seq) AS reason " // rp.seq로 정렬하면 날짜 컬럼 없어도 안전합니다.
+	               + "    LISTAGG(rp.reason, ', ') WITHIN GROUP (ORDER BY rp.seq) AS reason " //LISTAGG: 여러 개의 신고 사유를 한 줄로(사유1, 사유2...) 합쳐줍니다.
 	               + "FROM reply r "
 	               + "JOIN members m ON r.member_id = m.id "
 	               + "JOIN report rp ON r.seq = rp.reply_seq "
